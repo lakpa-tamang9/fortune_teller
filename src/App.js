980e8dbf1867b8801e84c71b34bf3a2df007c34a
lakpa-tamang9 <Ld.tamang25@gmail.com>
2023-01-03 13:45:53 +0900

    feat: Modify div propertu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+// import React, { useState } from "react";
+// import './App.css';
+
+// function App() {
+//   const [message, setMessage] = useState('');
+//   const [response, setResponse] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     fetch('http://localhost:3000', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ message }),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => setResponse(data.message));
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1> A.I 새점 -지능디자인</h1>
+//       <form onSubmit={handleSubmit}>
+//         <textarea 
+//           value={message}
+//           placeholder = "개인 정보 입력하세요... "
+//           onChange={(e) => setMessage(e.target.value)}
+//         ></textarea>
+//         <button type = "submit">submit</button>
+//       </form>
+//       <div>{response}</div>
+//       </div>
+//   );
+// }
+
+// export default App
+
 import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [language, setLanguage] = useState('english'); // default language is English
   const [message, setMessage] = useState('');
   const [response, setResponse] = useState('');
+
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,7 +55,7 @@ function App() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ message }),
+      body: JSON.stringify({ message, language }), // send the selected language to the server
     })
       .then((res) => res.json())
       .then((data) => setResponse(data.message));
@@ -20,17 +63,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1> A.I 새점 -지능디자인</h1>
+      <h1 style={{fontSize: '24px', color: 'blue', textAlign: 'center'}}> A.I 새점 -지능디자인</h1>
       <form onSubmit={handleSubmit}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <label>
+          Select Language:
+          <select value={language} onChange={handleLanguageChange}>
+            <option value="english">English</option>
+            <option value="korean">Korean</option>
+          </select>
+        </label>
         <textarea 
           value={message}
           placeholder = "개인 정보 입력하세요... "
           onChange={(e) => setMessage(e.target.value)}
         ></textarea>
-        <button type = "submit">submit</button>
+        </div>
+        <button type="submit">submit</button>
       </form>
       <div>{response}</div>
-      </div>
+    </div>
   );
 }
 
