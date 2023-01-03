ed51b1a0d7cbeaec948456d48c4cfa8ab22c751a
lakpa-tamang9 <Ld.tamang25@gmail.com>
2023-01-03 13:55:58 +0900

    chore: Modify h1 css val

--- src/App.js
+++ src/App.js
@@ -1,41 +1,3 @@
-// import React, { useState } from "react";
-// import './App.css';
-
-// function App() {
-//   const [message, setMessage] = useState('');
-//   const [response, setResponse] = useState('');
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     fetch('http://localhost:3000', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ message }),
-//     })
-//       .then((res) => res.json())
-//       .then((data) => setResponse(data.message));
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1> A.I 새점 -지능디자인</h1>
-//       <form onSubmit={handleSubmit}>
-//         <textarea 
-//           value={message}
-//           placeholder = "개인 정보 입력하세요... "
-//           onChange={(e) => setMessage(e.target.value)}
-//         ></textarea>
-//         <button type = "submit">submit</button>
-//       </form>
-//       <div>{response}</div>
-//       </div>
-//   );
-// }
-
-// export default App
-
 import React, { useState } from "react";
 import './App.css';
 
@@ -63,9 +25,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{fontSize: '24px', color: 'blue', textAlign: 'center'}}> A.I 새점 -지능디자인</h1>
+      <h1 style={{fontSize: '48px', color: 'brown', textAlign: 'center'}}> A.I 새점 -지능디자인</h1>
       <form onSubmit={handleSubmit}>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <div style={{display: 'grid;', flexDirection: 'row'}}>
         <label>
           Select Language:
           <select value={language} onChange={handleLanguageChange}>
@@ -75,7 +37,7 @@ function App() {
         </label>
         <textarea 
           value={message}
-          placeholder = "개인 정보 입력하세요... "
+          placeholder = "개인 정보 입력하세요 "
           onChange={(e) => setMessage(e.target.value)}
         ></textarea>
         </div>
